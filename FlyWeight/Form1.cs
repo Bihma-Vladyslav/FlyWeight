a626e3f42ece850573e2e76ae8a74f7d039bf8b5
VladLAS <vlad.bigma2004@gmail.com>
2023-04-11 22:55:47 +0300

    Initial commit

---
+++ FlyWeight/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlyWeight
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+        }
+        Graphics g;
+        SnowFlake[] s = new SnowFlake[100];
+        Param[] p = new Param[2];
+        ParamOneYearSnowFlake p1 = new ParamOneYearSnowFlake();
+        ParamTwoYearSnowFlake p2 = new ParamTwoYearSnowFlake();
+        int count = 0;
+        Random r = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (r.Next(0, 10) % 2 == 0)
+                s[count] = new SnowFlake(r.Next(10, 1400), r.Next(10, 1400), p1);
+            else
+                s[count] = new SnowFlake(r.Next(10, 1400), r.Next(10, 1400), p2);
+
+            g.DrawImage(s[count].p.pic, new Rectangle(s[count].x, s[count].y, s[count].p.h, 20));
+            if (count < 99)
+                count++;
+            else
+                timer1.Stop();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            foreach(SnowFlake sf in s)
+            {
+                g.FillRectangle(Brushes.White, sf.x, sf.y, sf.p.h, 20);
+                sf.fall();
+                g.DrawImage(sf.p.pic, new Rectangle(sf.x, sf.y, sf.p.h, 20));
+            }
+        }
+    }
+}
---
+++ FlyWeight/Haram.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FlyWeight
+{
+   public class Param
+    {
+        public int h;
+        public Bitmap pic;
+    }
+    public class ParamOneYearSnowFlake : Param
+    {
+        public ParamOneYearSnowFlake()
+        {
+            h = 50;
+            pic = new Bitmap(@"D:\Завантаження диск D\11.jpg");
+        }
+    }
+    public class ParamTwoYearSnowFlake : Param
+    {
+        public ParamTwoYearSnowFlake()
+        {
+            h = 100;
+            pic = new Bitmap(@"D:\Завантаження диск D\12.jpg");
+        }
+    }
+    public class SnowFlake
+    {
+        public int x, y;
+        public int x0, wind;
+        public Random r;
+        public Param p;
+        //------------
+        SnowFlake(int x0, int wind, Param p, Random r)
+        {
+            this.x0 = x0;
+            this.wind = wind;
+            this.p = p;
+            this.r = r;
+            this.y = r.Next(0, 1000);
+        }
+        public void fall()
+        {
+            if (y > 1000)
+            {
+                y = r.Next(0, 50);
+                x0 += r.Next(-15, 15);
+                y += 25;
+                x = x0 + wind * (int)Math.Sin(y);
+            }
+        }
+        public SnowFlake()
+        {
+            // h = 10;
+            x = 0; y = 0;
+
+        }
+        /* public SnowFlake(int x, int y, int ah, Bitmap apic)
+         {
+             h = ah;
+             pic = apic;
+             this.x = x;
+             this.y = y;
+         }*/
+        public SnowFlake(int x, int y, Param p)
+        {
+            this.p = p;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
